05df78fab6225ed9c38f25807eab3d7d246d9e1d
colafusion <-->
2022-02-06 14:25:16 +0000

    updated database logic, server logic done too - has searching for both treatment and patient records currently. No POSTs.

--- server.js
+++ server.js
@@ -1,93 +1,74 @@
-var express = require("express")
-var cors = require('cors');
-var bodyParser = require("body-parser");
-var db = require("./database.js")
+const express = require("express")
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const db = require("./database.js")
+const data = require("./server_setup_data.json");
 
-var app = express()
+
+const app = express()
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Server port
-var HTTP_PORT = 8080 
+const HTTP_PORT = 8080
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on http://localhost:%PORT%".replace("%PORT%", HTTP_PORT))
 });
 
 
 // Root endpoint
 app.get("/", (req, res, next) => {
-   res.json({"message":"Ok"})
+    res.json({"message": "Ok"})
 });
 
+for (const [key, values] of Object.entries(data)) {
 
-
-// list all books
-app.get("/books", (req, res, next) => {
-    let sql = `SELECT Name name FROM book ORDER BY name`;
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-
-// Get a single book by name
-app.get("/books/:name", (req, res, next) => {
-    var sql = "select * from book where name = ?"
-    var params = [req.params.name]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-// Create a new book
-app.post("/book/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.name){
-        errors.push("Name for book not specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-
-    var data = {
-        name: req.body.name,
-    }
-    var sql ='INSERT INTO book (name) VALUES (?)'
-    var params =[data.name]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
+    app.get(`/${key}/`, (req, res, next) => {
+        let sql = `SELECT * FROM ${key} ORDER BY ${values[0]}`;
+        console.log(sql)
+        const params = []
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({"error": err.message});
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        });
     });
-});
+
+    app.get(`/${key}/search/`, (req, res, next) => {
+        let queries = {}
+        values.map(val => queries[val] = req.query[val])
+
+        console.log(queries)
+        let valid_queries = {}
+        Object.keys(queries).map(key => (queries[key] ? key : null)).filter(val => val != null).map(key => valid_queries[key] = queries[key])
+
+        console.log(valid_queries)
+        let sql = `SELECT * FROM patients WHERE ${Object.keys(valid_queries).map(key => `${key} = ?`).join(' AND ')}`
+        console.log(sql, Object.values(valid_queries))
+        db.get(sql, Object.values(valid_queries), (err, row) => {
+            if (err) {
+                res.status(400).json({"error": err.message});
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": row
+            })
+        });
+    });
+}
 
 
 // Default response for any other request
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404);
 });
