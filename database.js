69ff0ad2e0666f8663cd454bc203f6dfe6cd7aca
colafusion <-->
2022-02-10 07:09:12 +0000

    --

--- database.js
+++ database.js
@@ -7,9 +7,12 @@ const db = new sqlite3.Database('medical_data.db', function (err) {
         throw err
     } else console.log('Connected to the medical_data database.');
 });
-
+//add the data to the database
+//json format:
+//table_name:create table statement, [list of columns in table], [[nested lists of rows]]
 db.serialize(() => {
     for (const [key, values] of Object.entries(data)) {
+        //first create the tables. Will throw errors to console if already present, but will work fine.
         db.run(values[0], function (err) {
                 if (err) {
                     console.error(err)
@@ -17,10 +20,15 @@ db.serialize(() => {
                     console.log(`${key} table created`);
                 }
             }
+            //then bulk insert the data
+            //this little set of logic basically states the columns to be added (and their order), then maps the applicable number of values. e.g.:
+            //INSERT INTO allergies (allergy_id,description) VALUES(?,?),(?,?),(?,?) [ 1, 'None', 2, 'Hayfever', 3, 'Hayfever - Acute' ]
+            //note the flattened list of rows, as sqlite doesn't like nested lists
         ).run(`INSERT INTO ${key} (${values[1]}) VALUES${values[2].map(row => `(${row.map(()=>'?').join(',')})`).join(',')}`, values[2].flat(), function (err) {
             if (err) {
                 console.error(`INSERT INTO ${key} (${values[1]}) VALUES${values[2].map(row => `(${row.map(()=>'?').join(',')})`).join(',')}`, values[2].flat(), err)
             } else {
+                console.log(`INSERT INTO ${key} (${values[1]}) VALUES${values[2].map(row => `(${row.map(()=>'?').join(',')})`).join(',')}`, values[2].flat())
                 console.log('data added')
             }
         })
--- server.js
+++ server.js
@@ -21,16 +21,18 @@ app.listen(HTTP_PORT, () => {
 
 
 // Root endpoint
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
     res.json({"message": "Ok"})
 });
-
+//setup the various GET options
+//we iterate through a dict with database table_name:[list of columns in table] format
 for (const [key, values] of Object.entries(data)) {
-
-    app.get(`/${key}/`, (req, res, next) => {
+    app.get(`/${key}/`, (req, res) => {
+        //key = table name, values[0] = first item in the list (i.e. the primary key)
         let sql = `SELECT * FROM ${key} ORDER BY ${values[0]}`;
         console.log(sql)
         const params = []
+        //perform the sql query
         db.all(sql, params, (err, rows) => {
             if (err) {
                 res.status(400).json({"error": err.message});
@@ -42,33 +44,70 @@ for (const [key, values] of Object.entries(data)) {
             })
         });
     });
-
-    app.get(`/${key}/search/`, (req, res, next) => {
+    //table search
+    app.get(`/${key}/search/`, (req, res) => {
+        //map the values to the queries in url. Insensitive to location in url (i.e. later columns can come before earlier ones - it'll query it fine either way)
         let queries = {}
         values.map(val => queries[val] = req.query[val])
 
         console.log(queries)
         let valid_queries = {}
+        //filters the list to get ones that are valid queries, i.e. not just 'primary_key=' etc
         Object.keys(queries).map(key => (queries[key] ? key : null)).filter(val => val != null).map(key => valid_queries[key] = queries[key])
 
         console.log(valid_queries)
-        let sql = `SELECT * FROM patients WHERE ${Object.keys(valid_queries).map(key => `${key} = ?`).join(' AND ')}`
+        //do the sql query, mapping the number of queries present to the number of ? required in the sql statement on the fly (+ ANDs)
+        let sql = `SELECT * FROM ${key} WHERE ${Object.keys(valid_queries).map(key => `${key} = ?`).join(' AND ')}`
         console.log(sql, Object.values(valid_queries))
         db.get(sql, Object.values(valid_queries), (err, row) => {
             if (err) {
                 res.status(400).json({"error": err.message});
                 return;
             }
-            res.json({
-                "message": "success",
-                "data": row
-            })
+            //if it's not an array, return it as an array anyway. Easier to handle.
+            //Useful in the cases where the query only returns one row of values
+            if(!Array.isArray(row)){
+                res.json({
+                    "message": "success",
+                    "data": [row]
+                })
+            }
+            else{
+                res.json({
+                    "message": "success",
+                    "data": row
+                })
+            }
         });
     });
 }
+//add to the patients table
+app.post("/patients/", (req, res) => {
+    console.log(req.body)
+    //get the request data
+    const req_data = {
+        name: req.body,
+    };
+    //note this assumes that the json order given to it is the same as in data['patients']
+    //could cause issues if the post req's dict is in a different order
+    const sql = `INSERT INTO patients(${data['patients']}) VALUES(${data['patients'].map(() => '?').join(',')})`;
+    const params = Object.values(req_data.name);
+    console.log(sql,params)
+    db.run(sql, params, function (err) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": req_data,
+        })
+    });
+});
 
 
 // Default response for any other request
 app.use(function (req, res) {
+   // console.log(req)
     res.status(404);
 });
